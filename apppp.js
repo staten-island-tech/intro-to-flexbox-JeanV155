ebf523e2db65c56723dde899b1d181d81e15daa9
JeanV155 <carlosgutierrez203074@gmail.com>
2025-09-29 12:24:14 -0400

    gdhtjndf nctu

---
+++ apppp.js
@@ -0,0 +1,14 @@
+const number1 = 8
+const number2 = 2
+const number3 = 2
+const number4 = 9
+
+if ((number1 || bowl2 && bowl1 < bowl3) || (bowl1 < bowl2 && bowl1 > bowl3)) {
+  mamaBowl = bowl1;
+} else if ((bowl2 > bowl1 && bowl2 < bowl3) || (bowl2 < bowl1 && bowl2 > bowl3)) {
+  mamaBowl = bowl2;
+} else {
+  mamaBowl = bowl3;
+}
+
+console.log("Mama Bear's bowl weighs:", mamaBowl);
