667edf775917e0986ce2a1d6c924d46d613a331a
JeanV155 <carlosgutierrez203074@gmail.com>
2025-09-30 12:34:25 -0400

    tedheded

--- apppp.js
+++ apppp.js
@@ -1,4 +1,4 @@
-const number1 = 8
+//const number1 = 8
 const number2 = 2
 const number3 = 2
 const number4 = 9
@@ -9,6 +9,9 @@ if ((number1 || bowl2 && bowl1 < bowl3) || (bowl1 < bowl2 && bowl1 > bowl3)) {
   mamaBowl = bowl2;
 } else {
   mamaBowl = bowl3;
-}
+}//
 
-console.log("Mama Bear's bowl weighs:", mamaBowl);
+console.log("Mama Bear's bowl weighs:", mamaBowl); 
+
+
+const sentence
---
+++ apppppppppp.js
@@ -0,0 +1,12 @@
+const sentence = "edward is cool and jean is a problem"
+console.log(x); 
+let T = 0
+let S = 0
+function language(sentence){
+    for( let i = 0; i< sentence.length; i++){
+        if(sentence[i] === "s" || sentence[i] === "S") {
+            s = s + 1;
+        } else if (sentence[i])
+    }
+
+}
