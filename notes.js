7e2566d7b6e9c486d640016275c2e4cbeb0a191c
JeanV155 <carlosgutierrez203074@gmail.com>
2025-10-06 11:49:00 -0400

    x

---
+++ notes.js
@@ -0,0 +1,11 @@
+const lonmdon = {
+    firstName: "london",
+    lastName: "Valentine",
+    graduated: "false",
+    sibilngs: ["paris", "rin"],
+    age: function () {
+        return year  
+    },
+};
+
+london.sibilngs.includes("paris")
