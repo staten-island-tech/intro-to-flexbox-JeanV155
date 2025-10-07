afe571f2ea5a39f4854e5f67186b625995c144be
JeanV155 <carlosgutierrez203074@gmail.com>
2025-10-07 12:30:51 -0400

    fgrhfdfhddfhhdfdhftfjh

--- dom.js
+++ dom.js
@@ -1,13 +1,12 @@
 const button = document.querySelector(".btn");
+
+function makeRGB() {
 const box = document.querySelector(".box") ;
-function makeRGB(event) {
-
-
- 
 let r = Math.floor(Math.random()* 256)
 let g = Math.floor(Math.random()* 256)
 let b = Math.floor(Math.random()* 256)
+console.log("hello")
 
-box.style.backroundcolor= 'rgb(${r}, ${g}, ${b}';
+box.style.backgroundColor= `rgb(${r}, ${g}, ${b}`;
 }
 button.addEventListener("click", makeRGB);
