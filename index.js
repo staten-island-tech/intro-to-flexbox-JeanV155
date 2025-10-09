04d6c2b367f88d8523aa669ab11c329eb6f2810a
JeanV155 <carlosgutierrez203074@gmail.com>
2025-10-09 12:30:16 -0400

    gghghghhytuiop

---
+++ index.js
@@ -0,0 +1,212 @@
+const House = [
+  {
+    id: 1,
+    name: "House 1",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13316/images-resized/Woodsons-Reserve---Executive_Valen-Modern-Farmhouse_Front-Elevetion_05_1920.jpg",
+    type: "4 bedroom",
+    area: "Westchester",
+    category: "Luxury",
+    price: 950000
+  },
+  {
+    id: 2,
+    name: "House 2",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13316/images-resized/Modern_Farmhouse_Exterior_01_1920.jpg",
+    type: "3 bedroom",
+    area: "Rochester",
+    category: "Modern",
+    price: 620000
+  },
+  {
+    id: 3,
+    name: "House 3",
+    instock: false,
+    img: "https://cdn.tollbrothers.com/communities/13844/images-resized/Exterior_01_1920.jpg",
+    type: "5 bedroom",
+    area: "Albany",
+    category: "Family",
+    price: 780000
+  },
+  {
+    id: 4,
+    name: "House 4",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13316/images-resized/Traditional_Exterior_01_1920.jpg",
+    type: "4 bedroom",
+    area: "Syracuse",
+    category: "Classic",
+    price: 710000
+  },
+  {
+    id: 5,
+    name: "House 5",
+    instock: false,
+    img: "https://cdn.tollbrothers.com/communities/13619/images-resized/Exterior_01_1920.jpg",
+    type: "3 bedroom",
+    area: "Buffalo",
+    category: "Beach",
+    price: 540000
+  },
+  {
+    id: 6,
+    name: "House 6",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13316/images-resized/Contemporary_Exterior_01_1920.jpg",
+    type: "2 bedroom",
+    area: "Ithaca",
+    category: "Modern",
+    price: 480000
+  },
+  {
+    id: 7,
+    name: "House 7",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13844/images-resized/Craftsman_Exterior_01_1920.jpg",
+    type: "4 bedroom",
+    area: "Poughkeepsie",
+    category: "Craftsman",
+    price: 690000
+  },
+  {
+    id: 8,
+    name: "House 8",
+    instock: false,
+    img: "https://cdn.tollbrothers.com/communities/13619/images-resized/Modern_Exterior_01_1920.jpg",
+    type: "3 bedroom",
+    area: "Binghamton",
+    category: "Modern",
+    price: 560000
+  },
+  {
+    id: 9,
+    name: "House 9",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13316/images-resized/Colonial_Exterior_01_1920.jpg",
+    type: "5 bedroom",
+    area: "Schenectady",
+    category: "Colonial",
+    price: 820000
+  },
+  {
+    id: 10,
+    name: "House 10",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13844/images-resized/Ranch_Exterior_01_1920.jpg",
+    type: "3 bedroom",
+    area: "Troy",
+    category: "Ranch",
+    price: 580000
+  },
+  {
+    id: 11,
+    name: "House 11",
+    instock: false,
+    img: "https://cdn.tollbrothers.com/communities/13619/images-resized/Tudor_Exterior_01_1920.jpg",
+    type: "4 bedroom",
+    area: "Jamestown",
+    category: "Tudor",
+    price: 760000
+  },
+  {
+    id: 12,
+    name: "House 12",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13316/images-resized/Victorian_Exterior_01_1920.jpg",
+    type: "6 bedroom",
+    area: "Newburgh",
+    category: "Victorian",
+    price: 990000
+  },
+  {
+    id: 13,
+    name: "House 13",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13844/images-resized/Beach_House_Exterior_01_1920.jpg",
+    type: "3 bedroom",
+    area: "Cortland",
+    category: "Beach",
+    price: 610000
+  },
+  {
+    id: 14,
+    name: "House 14",
+    instock: false,
+    img: "https://cdn.tollbrothers.com/communities/13619/images-resized/Mountain_Exterior_01_1920.jpg",
+    type: "4 bedroom",
+    area: "Lake Placid",
+    category: "Mountain",
+    price: 870000
+  },
+  {
+    id: 15,
+    name: "House 15",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13316/images-resized/Suburban_Exterior_01_1920.jpg",
+    type: "5 bedroom",
+    area: "Elmira",
+    category: "Family",
+    price: 760000
+  },
+  {
+    id: 16,
+    name: "House 16",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13844/images-resized/Modern_Farmhouse_Exterior_02_1920.jpg",
+    type: "4 bedroom",
+    area: "Kingston",
+    category: "Modern",
+    price: 730000
+  },
+  {
+    id: 17,
+    name: "House 17",
+    instock: false,
+    img: "https://cdn.tollbrothers.com/communities/13619/images-resized/Contemporary_Exterior_02_1920.jpg",
+    type: "3 bedroom",
+    area: "Glens Falls",
+    category: "Contemporary",
+    price: 590000
+  },
+  {
+    id: 18,
+    name: "House 18",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13316/images-resized/Traditional_Exterior_02_1920.jpg",
+    type: "4 bedroom",
+    area: "Ogdensburg",
+    category: "Classic",
+    price: 700000
+  },
+  {
+    id: 19,
+    name: "House 19",
+    instock: true,
+    img: "https://cdn.tollbrothers.com/communities/13844/images-resized/Colonial_Exterior_02_1920.jpg",
+    type: "5 bedroom",
+    area: "Hudson",
+    category: "Colonial",
+    price: 850000
+  },
+  {
+    id: 20,
+    name: "House 20",
+    instock: false,
+    img: "https://cdn.tollbrothers.com/communities/13619/images-resized/Ranch_Exterior_02_1920.jpg",
+    type: "3 bedroom",
+    area: "Geneva",
+    category: "Ranch",
+    price: 560000
+  }
+];
+function inject(House) {
+  DOMSelectors.display.insertAdjacentHTML(
+      `<div class="display-card">${House.url}
+      <h2 class="display-artist">${House.type}</h2>
+      <h3 class="display-album">${House.name}</h3>
+      <button class="remove btn">Remove Album</button>
+    </div>`
+  );
+}
+inject(House[0]);
